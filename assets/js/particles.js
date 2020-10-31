acaf66d753990edc56b100dea7089086a2dab138
Farid Arif <72960215+faridarif@users.noreply.github.com>
2020-10-31 15:12:54 +0800

    Update particles.js

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -66,7 +66,7 @@ var pJS = function(tag_id, params){
       },
       line_linked: {
         enable: true,
-        distance: 150,
+        distance: 200,
         color: '#fff',
         opacity: 1,
         width: 1
